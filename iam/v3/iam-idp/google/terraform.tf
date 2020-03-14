7974f8b7c091f72d5ea1b66768a3ee00e8369301
yclian <foss@yclian.com>
2020-03-14 12:42:30 +0800

    Add basic setup of Google IDP with a sample trust relationship (assume role) policy.

---
+++ iam/v3/iam-idp/google/terraform.tf
@@ -0,0 +1,10 @@
+# v3/iam-idp/google
+
+variable "aws_parent_account_id" {}
+variable "path_saml_metadata" { default = "./saml.xml" }
+
+
+resource "aws_iam_saml_provider" "google" {
+  name = "Google"
+  saml_metadata_document = file(var.path_saml_metadata)
+}
